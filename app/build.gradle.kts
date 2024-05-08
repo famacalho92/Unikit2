9af97c6617cc3d10c18a691d0266b2117d7cf54a
famacalho <56416895+famacalho@users.noreply.github.com>
2024-05-08 15:14:39 -0500

    27
    27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.unikit"
-        minSdk = 29
+        minSdk = 27
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
